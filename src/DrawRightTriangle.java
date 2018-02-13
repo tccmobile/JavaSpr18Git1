695903a7f4e4420b50c462a86800b70316dd41e6
drwillsmith <drwillsmith@gmail.com>
2018-02-13 09:16:45 -0600

    Initial commit

---
+++ src/DrawRightTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DrawRightTriangle {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        char triangleChar;
+        int triangleHeight;
+
+        System.out.println("Enter a character:");
+        triangleChar = scnr.next().charAt(0);
+
+        System.out.println("Enter triangle height:");
+        triangleHeight = scnr.nextInt();
+        System.out.println("");
+
+        System.out.println("*" + " ");
+        System.out.println("*" + " " + "*" + " ");
+        System.out.println("*" + " " + "*" + " " + "*" + " ");
+    }
+}
